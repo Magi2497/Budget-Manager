5d0bdf4cc551c1d7f3ce303bab70e9aa586b8a03
Magi2497 <magipayerasasndreu@gmail.com>
2025-07-01 17:44:28 +0200

    conected context with reducer and app

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,31 +1,9 @@
-import { useReducer, createContext, type Dispatch, type ReactNode } from 'react'
-import {
-  budgetReducer,
-  initialState,
-  type BudgetActions,
-  type BudgetState,
-} from '../reducers/budget-reducer'
+import { createContext, type Dispatch } from 'react'
+import type { BudgetActions, BudgetState } from '../reducers/budget-reducer'
 
-type BudgetContextPorps = {
+type BudgetContextProps = {
   state: BudgetState
   dispatch: Dispatch<BudgetActions>
 }
 
-type BudgetProviderProps = {
-  children: ReactNode
-}
-export const BudgetContext = createContext<BudgetContextPorps>(null!)
-
-export const BudgetProvider = ({ children }: BudgetProviderProps) => {
-  const [state, dispatch] = useReducer(budgetReducer, initialState)
-  return (
-    <BudgetContext.Provider
-      value={{
-        state,
-        dispatch,
-      }}
-    >
-      {children}
-    </BudgetContext.Provider>
-  )
-}
+export const BudgetContext = createContext<BudgetContextProps>(null!)
